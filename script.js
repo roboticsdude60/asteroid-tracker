415f0f368d88efee83136e3391227f6a0908c5d4
Glitch (glitch-hello-website) <none>
2022-10-15 22:31:19 +0000

    🎁🚜 Checkpoint ./script.js:48973841/8052 ./index.html:48973841/420

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const canvas = document.getElementById("myCanvas");
-canvas.width  = visualViewport.width;
+canvas.width = visualViewport.width;
 canvas.height = 300;
 const centerCrossAxis = canvas.height / 2;
 
@@ -13,9 +13,8 @@ function drawEarth() {
   // label earth
   ctx.fillStyle = "white";
   ctx.font = "18px Arial";
-  ctx.fillText("Earth", 20, centerCrossAxis)
+  ctx.fillText("Earth", 20, centerCrossAxis);
 }
-
 
 // give me the lunar miss distance
 // and the average of the estimated_diameter_min and estimated_diameter_max in kilometers
@@ -28,7 +27,7 @@ function drawAsteroid(name, distance, diameter) {
 }
 
 drawEarth();
-drawAsteroid('465633 (2009 JR5)',117.7689258646, 0.2251930467);
+drawAsteroid("465633 (2009 JR5)", 117.7689258646, 0.2251930467);
 
 /* 
 Make the "Click me!" button move when the visitor clicks it:
@@ -37,13 +36,11 @@ Make the "Click me!" button move when the visitor clicks it:
 const btn = document.querySelector("button"); // Get the button from the page
 // Detect clicks on the button
 if (btn) {
-  btn.onclick = function() {
+  btn.onclick = function () {
     // The JS works in conjunction with the 'dipped' code in style.css
     btn.classList.toggle("dipped");
   };
 }
-
-
 
 /*
 API format:
@@ -54,30 +51,40 @@ API_KEY: N/A
 */
 
 const asteroidAPIURL = "";
-function setAsteroidAPIURL(start_date, end_date) {
-  const regex = /\d{4}-[01-12]{2}-[01-31]{2}/
-  if (regex.test(start_date) == false || regex.test(end_date) == false){
-    throw 'START_DATE or END_DATE is not formatted in YYYY-MM-DD format'
+function setAsteroidAPIURL() {
+  const start_date = document.getElementById("start_date").innerHTML;
+  const end_date = document.getElementById("end_date").innerHTML;
+  const regex = /\d{4}-[01-12]{2}-[01-31]{2}/;
+  if (regex.test(start_date) == false || regex.test(end_date) == false) {
+    throw "START_DATE or END_DATE is not formatted in YYYY-MM-DD format";
   }
-  asteroidAPIURL = "https://api.nasa.gov/neo/rest/v1/feed?start_date=" + start_date + "&end_date=" + end_date;
-  }
-
-
-function getAsteroidData(start_date, end_date) {
-  setAsteroidAPIURL(start_date, end_date);
-  fetch(asteroidAPIURL)
-    .then(function(response) {
-          if (response.satatus != 200) {
-            return {
-              text: "Error calling asteroid API: " + response.statusText
-            }
-          }
-          return response.json();
-      }).then (function(json) {
-    organizeAsteroidData(json);
-  })
+  asteroidAPIURL =
+    "https://api.nasa.gov/neo/rest/v1/feed?start_date=" +
+    start_date +
+    "&end_date=" +
+    end_date;
 }
 
+function getAsteroidData() {
+  try {
+    setAsteroidAPIURL();
+
+    fetch(asteroidAPIURL)
+      .then(function (response) {
+        if (response.satatus != 200) {
+          return {
+            text: "Error calling asteroid API: " + response.statusText,
+          };
+        }
+        return response.json();
+      })
+      .then(function (json) {
+        organizeAsteroidData(json);
+      });
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 /*
 Will organize all the data from the json to:
@@ -92,29 +99,28 @@ relative_velocity: float(km/sec)
 //what else do we want?
 }
 */
-function organizeAsteroidData(json_data){
+function organizeAsteroidData(json_data) {
+  console.log(`organizing asteroid data: ${JSON.stringify(json_data)}`);
   const asteroids = [];
   const near_earth_objects = json_data.near_earth_objects;
   near_earth_objects.forEach((object) => {
     const asteroid = {};
     asteroid.name = object.name;
-    asteroid.estimated_diameter_min = object.estimated_diameter.kilometers.estimated_diameter_min;
-    asteroid.estimated_diameter_max = object.estimated_diameter.kilometers.estimated_diameter_max;
-    asteroid.is_potentially_hazardous_asteroid = object.is_potentially_hazardous_asteroid;
-    asteroid.miss_distance = object.close_approach_data.miss_distance.kilometers;
-    asteroid.relative_velocity = object.close_approach_data.relative_velocity.kilometers_per_second;
-    asteroids.push(asteroid); 
+    asteroid.estimated_diameter_min =
+      object.estimated_diameter.kilometers.estimated_diameter_min;
+    asteroid.estimated_diameter_max =
+      object.estimated_diameter.kilometers.estimated_diameter_max;
+    asteroid.is_potentially_hazardous_asteroid =
+      object.is_potentially_hazardous_asteroid;
+    asteroid.miss_distance =
+      object.close_approach_data.miss_distance.kilometers;
+    asteroid.relative_velocity =
+      object.close_approach_data.relative_velocity.kilometers_per_second;
+    asteroids.push(asteroid);
   });
   console.log(json_data);
-  
 }
 
-// This is a single line JS comment
-/*
-This is a comment that can span multiple lines 
-- use comments to make your own notes!
-*/
-
-
-
-
+document
+  .getElementById("test_button")
+  .addEventListener("click", getAsteroidData);
